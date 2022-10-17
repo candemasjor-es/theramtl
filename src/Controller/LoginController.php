7f1e508e3bee17fc677232faadceb62d023d47b9
candemasjor-es <camilo_mascarell_jorques@candemasjor.es>
2022-10-17 14:58:00 +0200

    usuario diseño y error 404

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -14,6 +14,7 @@ class LoginController extends AbstractController
     {
         $error = $authenticationUtils->getLastAuthenticationError();
         $lastUsername = $authenticationUtils->getLastUsername();
+            
         return $this->render('login/index.html.twig', [
             'last_username' => $lastUsername,
             'error'         => $error,
